b10f3047567fcedeee45ae0624abf6738bb838ca
MatteoVicenzino <115220969+MatteoVicenzino@users.noreply.github.com>
2022-11-22 12:25:04 +0000

    v3, ho considerato il caso in cui il csv sia vuoto e quello in cui la somma è zero. Il voto è 8

--- Lezione3/somma_csv.py
+++ Lezione3/somma_csv.py
@@ -2,12 +2,16 @@ def sum_csv(file_name):
     elementi = []
     somma = 0.0
     file_sales = open(file_name, 'r')
+    if file_sales == []:
+        return None
     for line in file_sales:
         elementi = line.split(',')
         if elementi[0] != 'Date':
             valore = float(elementi[1])
             # print('{}'.format(valore))
             somma = somma + valore
+    if somma == 0.0:
+        return None
     return somma
 
 # risultato = sum_csv('shampoo_sales.csv')
