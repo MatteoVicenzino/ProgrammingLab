3d42417505ab707929eef1db9b2bd0aba55e77bd
MatteoVicenzino <115220969+MatteoVicenzino@users.noreply.github.com>
2022-11-29 11:22:13 +0000

    v1, es 5a funziona, voto 10, es 5b ancora da fare!

---
+++ Lezione5/eccezioni_a.py
@@ -0,0 +1,21 @@
+class CSVFile():
+    
+    def __init__(self, name):
+        self.name = name
+
+    def get_data(self):
+        try:
+            my_file = open(self.name, 'r')
+            element = []
+            if my_file == []:
+                return None
+            for line in my_file:
+                element1 = line.strip("\n").split(',')
+                if element1[0] != 'Date':
+                    element.append(element1)
+            return element
+        except Exception as e:
+            print('ho avuto un Errore Generico del tipo {}'.format(e))
+
+appoggio = CSVFile('shampoo_sales.csv')
+print('{}'.format(appoggio.get_data()))
