a856e4699377237283248634b5f31194e3317420
MatteoVicenzino <115220969+MatteoVicenzino@users.noreply.github.com>
2023-01-28 21:57:52 +0000

    v1, prima versione funzionante, non tiene conto dell'ordine con cui i vestiti vanno messi, e va sempre a buon fine perché l'automa riesce sempre a vestirsi

---
+++ Foglio2/es1_exc_sanit.py
@@ -0,0 +1,97 @@
+import random
+
+class Automa():
+
+    def __init__(self):
+        self.biancheria
+        self.calzini
+        self.maglia
+        self.pantaloni
+        self.calzatura
+      
+    #funzioni che vestono l'automa: ritornano 1 se l'azione è andata a buon fine
+    def biancheria(self):
+        self.biancheria = True
+        return 1
+        
+    def calzini(self):
+        self.calzini = True
+        return 1
+        
+    def maglia(self):
+        self.maglia = True
+        return 1
+        
+    def pantaloni(self):
+        self.pantaloni = True
+        return 1
+        
+    def calzatura(self):
+        self.calzatura = True
+        return 1
+
+
+#funzione esegui: gli dici che capo mettere e veste quel capo
+#ritornano ciò che ritorna il metodo chiamato
+def esegui(automa, capo):
+    
+    if capo == "biancheria":
+        print("mi metto la biancheria")
+        return(automa.biancheria())
+
+    if capo == "calzini":
+        print("mi metto i calzini")
+        return(automa.calzini())
+
+    if capo == "maglia":
+        print("mi metto la maglia")
+        return(automa.maglia())
+
+    if capo == "pantaloni":
+        print("mi metto i pantaloni")
+        return(automa.pantaloni())
+
+    if capo == "calzatura":
+        print("mi metto le calzature")
+        return(automa.calzatura())
+        
+    
+
+#esempio TEST all
+lista_capi = ["biancheria", "calzini", "maglia", "pantaloni", "calzatura"]
+
+#corpo dello script che, capo per capo chiama esegui
+vestito = False
+automa = Automa()
+while vestito != True:
+    capo = random.choice(lista_capi)
+
+    # (G1) gestire in caso il capo selezionato va contro la sequenza della procedura
+    #if(order_check(capo) != True):
+    
+    try: #provo a vestire il capo selezionato
+        done = esegui(automa, capo)
+
+        if done == 1: #se il capo è stato indossato
+            #tolgo il capo dalla lista per non tornare a sceglierlo casualmente
+            lista_capi.remove(capo)
+        else: #se invece il capo NON è stato indossato
+            #gestisco il caso G2 in cui l'automa non riesce a vestirsi e ritorna 0
+            raise Exception("L'automa non è riuscito a mettersi capo {} per un problema hardware. ".format(capo))
+        
+            
+        
+    except Exception as e: #se esegui non va a buon fine
+        print("L'automa non è riuscito a mettersi capo {} per un problema software di tipo {}. ".format(capo, e))
+
+
+    #vestito diventa true quando:
+    #1 non ci sono più capi da mettere, l'automa si è messo tutto
+    if len(lista_capi)<1:
+        vestito = True
+        print("automa vestito correttamente")
+    
+
+    
+
+
