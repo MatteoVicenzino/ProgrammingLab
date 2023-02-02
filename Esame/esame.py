d67fdfb6beabadd941d2a192ad922e69aca3af33
MatteoVicenzino <115220969+MatteoVicenzino@users.noreply.github.com>
2023-02-02 12:20:20 +0000

    v2, completato, esame.py è quello corretto

--- Esame/esame.py
+++ Esame/esame.py
@@ -21,19 +21,19 @@ class CSVTimeSeriesFile(CSVFile):
         except Exception as e:
             raise ExamException('Errore: file non leggibile o vuoto')
             
-        lista_di_liste = [] 
+        lista_di_liste = [] #creo lista vuotra che accoglierà le liste annidate
         for line in my_file:
             lista_annidata = line.strip('\n').split(',')
-            if lista_annidata[0] != 'epoch': 
+            if lista_annidata[0] != 'epoch': # salto intestazione tabella
                 try:
-                    #Converto gli epoch float in interi silenziosamente
+                    #Converto silenziosamente gli epoch float in interi
                     #stringa->float->int, perché non posso convertire
-                    #stringa '12.5' direttamente in intero, non rappresenta un int
+                    #stringa '12.5' -> int, non rappresenta un int
                     lista_annidata[0] = int(float(lista_annidata[0]))
                     lista_annidata[1] = float(lista_annidata[1])
                 except Exception as e:
                     #se epoch o temperatura non contengono valori numerici
-                    #la ocnversione a float non andrà a buon fine
+                    #la conversione a float non andrà a buon fine
                     #quindi ignoro quella riga
                     continue
                 lista_di_liste.append(lista_annidata)
@@ -41,34 +41,53 @@ class CSVTimeSeriesFile(CSVFile):
         #controllo per epoch fuori ordine o duplicato
         for i in range(len(lista_di_liste)-1): #scorro la lista fino al penultimo
             if lista_di_liste[i+1][0] <= lista_di_liste[i][0]:
-                raise ExamException('Errore: lista non ordinata o timestampo duplicato')
+                raise ExamException('Errore: lista non ordinata o timestamp duplicato')
 
         return lista_di_liste
 
 
-def diff(time_series):
-    
-    #lista liste in cui ogni listina contiene i valori di una giornata
-    lista_giornate = []
-    daily_values = []
+
+def day_arrangement(time_series):
+
+    lista_giornate = [] #creo lista di liste
+    daily_values = [] #contiene tutte le temperature di una singola giornate
     for i in range(len(time_series)):
-        if i == 0:
+        if i == 0: #caso primo valore: lo aggiungo a daily_values
             daily_values = [time_series[i][1]]
-        elif time_series[i][0] == time_series[i-1][0]: # se il giorno è uguale al precedente
-            daily_values.append(time_series[i][1])
+        elif time_series[i][0] == time_series[i-1][0]: 
+            #se il giorno è uguale al precedente
+            #aggiungo il valore ad una stessa daily_values
+            daily_values.append(time_series[i][1]) 
         else:
-            #se invece è diverso sovrascrivo listina
+            #se invece è diverso 
+            #sovrascrivo daily_values dopo averla aggiunta alla lista delle giornate
             lista_giornate.append(daily_values)
             daily_values = [time_series[i][1]]
-        if i == len(time_series)-1:
+        if i == len(time_series)-1: #caso ultimo valore: 
+            #aggiungo l'ultima giornata alla lista delle giornate
             lista_giornate.append(daily_values)
+            
+    return lista_giornate
+
+
+def compute_daily_max_difference(time_series):
+
+    #converto gli epoch da secondi in giorni
+    for item in time_series:
+        coppia = item
+        val_pre = coppia[0] #è il valore pre conversione
+        #dividendo per il numero di secondi in un giorno:
+        day_start_epoch = val_pre / 86400
+        #converto ad int, per toglire la parte decimale che rappresenta i secondi
+        coppia[0] = int(day_start_epoch)
+    #time_series ora contiene epoch in giorni
     
+    lista_giornate = day_arrangement(time_series) #temperature divise per giornata
     
-    differenze = []
+    differenze = [] #lista delle differenze che ritornerò alla fine
     for i in range(len(lista_giornate)):
-        
         if len(lista_giornate[i]) < 1: 
-            #Se non c'è almeno una misurazione, la differenza di quel giorno sarà None
+            #Se non c'è almeno una misurazione per giorno, la differenza di quel giorno sarà None
             differenze.append(None)
         else:
             escursione_termica = max(lista_giornate[i]) - min(lista_giornate[i])
@@ -76,26 +95,9 @@ def diff(time_series):
     return differenze
 
 
-def compute_daily_max_difference(time_series):
-    
-    
-    for item in time_series:
-        coppia = item
-        val_pre = coppia[0]
-        day_start_epoch = val_pre / 86400
-        coppia[0] = int(day_start_epoch)
-    #lista uguale a time_series ma con sec -> giorni
-    risultato = diff(time_series)
-    
-    return risultato
-
-
-
 
 time_series_file = CSVTimeSeriesFile(name='data.csv')
 time_series = time_series_file.get_data()
-
-
 differenze = compute_daily_max_difference(time_series)
 
 for item in differenze:
